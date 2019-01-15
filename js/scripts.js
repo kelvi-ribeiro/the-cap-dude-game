63b9c09735fdd98c74b64b19c2f8db66ef52f5f6
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-01-15 19:12:42 -0200

    :hankey: Programando dot para aparecer de tempo em tempo na tela

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,16 @@ const boyPosition = {
   } 
   
   var infoDebuggers = [];
+
+  var speedMove = 20;
+  var body = document.body,
+
+  html = document.documentElement;
+
+  var scrollHeight = 1000;
+
+  const tempoCriacaoNovoPoint = 5000
+
   
   var debuggerMode = false;
   $('#btn-handle-debugger').click(function(){
@@ -33,16 +43,41 @@ const boyPosition = {
     $('#btn-handle-debugger').text('Ligar Debuggers')
     return;
   }
+  function startCreatePointRed(){
+    setInterval(function(){
+      $('#point-red').remove()
+      $('#game-mode').append(
+        `
+        <div id="point-red" style="position:relative;top:${ Math.floor(Math.random() * (scrollHeight - 0) + 0)}px;left:${ Math.floor(Math.random() * (scrollHeight - 0) + 0)}px;">
+        </div>
+        `
+      )
+    },tempoCriacaoNovoPoint)
+    
+  }
   function enableGameMode(){
     $('#tutorial-mode').each(function() {
         $(this).remove();        
     });
+    startCreatePointRed()
     $('#game-mode').append(
         `  <div class="has-text-centered">          
-          <img id="img-boy" src="img/boy.png" alt="Boy">      
-        </div>            
-        `
-    )
+        <img id="img-boy" src="img/boy.png" alt="Boy">      
+        </div>                   
+        `      
+        )
+  }
+  function verifyAvatarPositionIsEqualToRedDot(){
+    const boyPosition = {
+      left:($('#img-boy').offset().left),
+      top:($('#img-boy').offset().top),
+    }
+    const dotPosition = {
+      left:($('#point-red').offset().left),
+      top:($('#point-red').offset().top),
+    }
+    console.log('boyPosition',boyPosition)
+    console.log('dotPosition',dotPosition)
   }
   $('#start-game').click(function(){
     enableGameMode()    
@@ -52,32 +87,19 @@ const boyPosition = {
     location.reload();
   });
 
-  $('#arrow-left').click(function(){      
-    $('#img-boy').css("right",boyPosition.right += 5)
-  });
-  $('#arrow-right').click(function(){
-    $('#img-boy').css("right",boyPosition.right -= 5)
-  });
-  $('#arrow-top').click(function(){
-    $('#img-boy').css("top",boyPosition.top += 5)
-  });
-  $('#arrow-top').click(function(){
-    $('#img-boy').css("top",boyPosition.top -= 5)
-  });
-  
-
 $("body").keydown(function(e) {
   if(e.keyCode == 37) { // left        
-    $('#img-boy').css("right",boyPosition.right += 5)
+    $('#img-boy').css("right",boyPosition.right += speedMove)
   }
   else if(e.keyCode == 39) { // right
-    $('#img-boy').css("right",boyPosition.right -= 5)
+    $('#img-boy').css("right",boyPosition.right -= speedMove)
   }else if(e.keyCode == 40){
-      if(($('#img-boy').offset().top - $(window).scrollTop() + 5) < 0) return;
-      if(($('#img-boy').offset().top - $(window).scrollTop() + 5) > $(window).height()) return;
-    $('#img-boy').css("top",boyPosition.top += 5)
+      if(($('#img-boy').offset().top - $(window).scrollTop() + speedMove) < 0) return;
+      if(($('#img-boy').offset().top - $(window).scrollTop() + speedMove) > $(window).height()) return;
+    $('#img-boy').css("top",boyPosition.top += speedMove)
   }else if(e.keyCode == 38){            
-    if(($('#img-boy').offset().top - $(window).scrollTop() - 5) < 0) return;
-    $('#img-boy').css("top",boyPosition.top -= 5)
+    if(($('#img-boy').offset().top - $(window).scrollTop() - speedMove) < 0) return;
+    $('#img-boy').css("top",boyPosition.top -= speedMove)
   }
+  verifyAvatarPositionIsEqualToRedDot()
 });
